34b3a1950dfe4618da7881c47647008ed32be6c4
Your Name <you@example.com>
2020-11-23 12:34:58 +0530

    first commit

---
+++ source/repos/WebAPICore/WebAPICore/Controllers/DepartmentsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPICore.Data;
+using WebAPICore.Models;
+
+namespace WebAPICore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly EmployeeContext _context;
+
+        public DepartmentsController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Departments
+        [HttpGet]
+        public IEnumerable<Department> GetDepartments()
+        {
+            return _context.Departments;
+        }
+
+        // GET: api/Departments/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDepartment([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var department = await _context.Departments.FindAsync(id);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(department);
+        }
+
+        // PUT: api/Departments/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDepartment([FromRoute] long id, [FromBody] Department department)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != department.DepartmentID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(department).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Departments
+        [HttpPost]
+        public async Task<IActionResult> PostDepartment([FromBody] Department department)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Departments.Add(department);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDepartment", new { id = department.DepartmentID }, department);
+        }
+
+        // DELETE: api/Departments/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDepartment([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var department = await _context.Departments.FindAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            _context.Departments.Remove(department);
+            await _context.SaveChangesAsync();
+
+            return Ok(department);
+        }
+
+        private bool DepartmentExists(long id)
+        {
+            return _context.Departments.Any(e => e.DepartmentID == id);
+        }
+    }
+}
---
+++ source/repos/WebAPICore/WebAPICore/Data/EmployeeContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPICore.Models;
+
+namespace WebAPICore.Data
+{
+    public class EmployeeContext: DbContext
+    {
+        public EmployeeContext(DbContextOptions options): base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
